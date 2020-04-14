9626b4dec6df42b2112c2cc68f518ed96e51c46c
Marc Cometa <menfcometa@gmail.com>
2020-04-14 11:33:55 +0800

    Updates

---
+++ precache-manifest.21e09498bdb0a295bcc4993db8290724.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb919ed4ffb95bc321a8e8b7dee87b9c",
+    "url": "/my-lms/index.html"
+  },
+  {
+    "revision": "7596dae03fae30498eb4",
+    "url": "/my-lms/static/css/2.fa81c199.chunk.css"
+  },
+  {
+    "revision": "e9d3ed211ee8142d2103",
+    "url": "/my-lms/static/css/main.2c9bf71c.chunk.css"
+  },
+  {
+    "revision": "7596dae03fae30498eb4",
+    "url": "/my-lms/static/js/2.b5e56961.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-lms/static/js/2.b5e56961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9d3ed211ee8142d2103",
+    "url": "/my-lms/static/js/main.0466e6f6.chunk.js"
+  },
+  {
+    "revision": "ec75409ef31e0d00f434",
+    "url": "/my-lms/static/js/runtime-main.77d2f49e.js"
+  }
+]);
